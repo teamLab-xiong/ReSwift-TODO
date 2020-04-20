03963c987aa02777a13d9a63a03b178ae798529b
Xiong Ju <ixongju@gmail.com>
2020-04-20 22:41:56 +0900

    create todo on other view controller

---
+++ ReSwift-TODO/CreateViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateViewController.swift
+//  ReSwift-TODO
+//
+//  Created by Xiong Ju 熊炬 on 2020/4/20.
+//  Copyright © 2020 Xiong Ju 熊炬. All rights reserved.
+//
+
+import UIKit
+
+class CreateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
