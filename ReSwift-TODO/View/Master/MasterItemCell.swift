fb75d3c339a3ac0ff72a0e874f60b82eceef23bf
Xiong Ju <ixongju@gmail.com>
2020-04-21 09:37:52 +0900

    use separated cell

---
+++ ReSwift-TODO/View/Master/MasterItemCell.swift
@@ -0,0 +1,25 @@
+//
+//  MasterItemCell.swift
+//  ReSwift-TODO
+//
+//  Created by Xiong Ju 熊炬 on 2020/4/21.
+//  Copyright © 2020 Xiong Ju 熊炬. All rights reserved.
+//
+
+import UIKit
+
+class MasterItemCell: UITableViewCell {
+    @IBOutlet fileprivate weak var titleLabel: UILabel!
+    @IBOutlet fileprivate weak var messageLabel: UILabel!
+    @IBOutlet fileprivate weak var dateLabel: UILabel!
+}
+
+extension MasterItemCell: Injectable {
+    func inject(_ dependency: TODO) {
+        titleLabel.text = dependency.title
+        messageLabel.text = dependency.message
+        dateLabel.text = dependency.date.description
+    }
+    
+    typealias Dependency = TODO
+}
